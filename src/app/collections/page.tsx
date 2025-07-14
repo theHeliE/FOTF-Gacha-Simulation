df22e1aee31308060fa8fc97f3daea56b5d1eff6
theHeliE <129796088+theHeliE@users.noreply.github.com>
2025-07-14 20:08:31 +0300

    ADD: navigation

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -6,22 +6,13 @@ import { ModeToggle } from "@/components/mode-toggle";
 import { ProgressDemo } from "@/components/progress";
 import { useRouter } from "next/navigation";
 
-export default function Home() {
+export default function Collections() {
   const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
 
   const handleProgressComplete = () => {
     setIsLoading(false);
   };
-  const handleGachaClick = (e: any) => {
-    e.preventDefault();
-    router.push("/gacha");
-  };
-  const handleCollectionsClick = (e: any) => {
-    e.preventDefault();
-    router.push("/collections");
-  };
-
   if (isLoading) {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -49,21 +40,6 @@ export default function Home() {
           <p className="text-lg text-muted-foreground mb-8">
             Your adventure begins here!
           </p>
-        </div>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <button
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            onClick={handleGachaClick}
-          >
-            Start Gacha
-          </button>
-          <button
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            onClick={handleCollectionsClick}
-          >
-            View Collection
-          </button>
         </div>
       </main>
 
